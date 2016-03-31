c8668e83e851272808d9c76703a2a3eab6e9afe4
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-03-31 16:50:26 +0900

    [Update] tableの情報は分離して、あくまで要素のみを扱わせた。

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
 
-function parseMeta(json){
-	return "<h3>"+json.name+"</h3>"+json.description+'\n';
-}
-
 function wrapTd(o){
 	if(o == undefined){
 		// undefinedではなく別の文字にしてもいいかも
-		return '<td style="color:red;">'+o+'</td>';
+		return '<td style="color:red;">'+o+'</td>';	
 	}
 	return "<td>"+o+"</td>";
+}
+
+function wrapTh(o){
+	return "<th>"+o+"</th>";
 }
 function parseElement( name, obj){
 	var result = wrapTd(name);
@@ -19,11 +19,11 @@ function parseElement( name, obj){
 	return result;
 }
 function parseElements(obj){
-	var result = '<table border="1">\n';
-	var col = ["name","Type",'isOptional','description'];
-	result += '<tr>';
+	var result = '<table>\n';
+	var col = ["name","Type",'isOptional','description']; 
+	result += '<tr>'; 
 	for(i = 0;i < col.length;i++){
-		result += wrapTd(col[i]);
+		result += wrapTh(col[i]);
 	}
 	result += '</tr>';
 
@@ -34,12 +34,22 @@ function parseElements(obj){
 	return result;
 }
 
-function parse(json){
-	var table = json.table;
-	var elements = json.elements;
 
-	var result = parseMeta(table);
-	result += parseElements(elements);
+
+function sqlArgument(name,obj){
+	if(!obj.isOptional){
+		return '`'+name+'` ' + obj.type+' not null';
+	} 
+	return '`'+name+'` ' + obj.type;
+}
+function createSQL( name, obj){
+	var result = '<pre><code class="lang-sql"> create table ';
+	result += name + '(';
+	for(key in obj){
+		result += ' ' + sqlArgument(key, obj[key])+',';
+	}
+	result = result.substring(0,result.length-1);
+	result += '); </code></pre>';
 	return result;
 }
 
@@ -47,13 +57,16 @@ module.exports = {
     blocks: {
 			jsoneon: {
 				process: function(block) {
-				if(block.args.length == 1){
-					var filepath = fs.realpathSync('./json/');
-					var json = require(filepath+'/'+block.args[0]);
-					return parse(json);
-				}
-				return "Error! You need argument filename";
+					if("filename" in block.kwargs){
+						var json = require(
+							fs.realpathSync('./json/')+'/'+block.kwargs.filename
+						);
+						var name = block.kwargs.name;
+						return parseElements(json);
+					}
+					return "";
 			}
 		}
 	}
 };
+
